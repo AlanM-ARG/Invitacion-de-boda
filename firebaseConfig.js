0e3dc0bd534b3945c5eef09c9b8444fd94b66c8d
Alan Morua <alan.morua@quintoimpacto.net>
2024-10-31 01:35:15 -0300

    vueapp

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.c
 
 // Configuración de Firebase usando valores de entorno directamente
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "defaultKey",
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "defaultDomain",
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "defaultProject",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "defaultBucket",
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "defaultSenderId",
-    appId: import.meta.env.VITE_FIREBASE_APP_ID || "defaultAppId",
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "defaultMeasurementId"
+    apiKey: import.meta.env.VUE_APP_FIREBASE_API_KEY || "defaultKey",
+    authDomain: import.meta.env.VUE_APP_FIREBASE_AUTH_DOMAIN || "defaultDomain",
+    projectId: import.meta.env.VUE_APP_FIREBASE_PROJECT_ID || "defaultProject",
+    storageBucket: import.meta.env.VUE_APP_FIREBASE_STORAGE_BUCKET || "defaultBucket",
+    messagingSenderId: import.meta.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID || "defaultSenderId",
+    appId: import.meta.env.VUE_APP_FIREBASE_APP_ID || "defaultAppId",
+    measurementId: import.meta.env.VUE_APP_FIREBASE_MEASUREMENT_ID || "defaultMeasurementId"
 };
 
 // Inicializar Firebase
