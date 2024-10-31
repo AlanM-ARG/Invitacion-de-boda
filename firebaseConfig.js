95ef2317f1d04c8887cc3d071147450b9479e62d
Alan Morua <alan.morua@quintoimpacto.net>
2024-10-31 01:07:19 -0300

    env2 prueba

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.c
 
 // Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-    apiKey: VUE_APP_FIREBASE_API_KEY,
-    authDomain: VUE_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: VUE_APP_FIREBASE_PROJECT_ID,
-    storageBucket: VUE_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: VUE_APP_FIREBASE_APP_ID,
-    measurementId: VUE_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializar Firebase
